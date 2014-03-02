ae2a782a7c61d21c3f9acf357629b60949afb5a8
Ben London <blondon@cs.umd.edu>
2014-03-02 16:22:57 -0500

    Removed mex file UGM_Infer_CTRBPC.c, since no longer necessary. Added UGM_Infer_CBP and UGM_Decode_CBP, which do convexified marginal inference and max-of-marginals decoding using Mark's implementations of BP.

---
+++ UGM/decode/UGM_Decode_CBP.m
@@ -0,0 +1,17 @@
+function  [nodeLabels] = UGM_Decode_CBP(convexity,nodePot,edgePot,edgeStruct,inferFunc,varargin)
+%
+% Decode convexified BP inference.
+%
+% convexity : scales modulus of convexity of entropy (approximation) by
+%				inverse scaling of log-potentials
+% nodePot : node potentials
+% edgePot : edge potentials
+% edgeStruct : edge struct
+% inferFunc : inference function
+
+assert(nargin >= 5, 'USAGE: UGM_Decode_CBP(convexity,nodePot,edgePot,edgeStruct,inferFunc)')
+
+assert(convexity > 0, 'In UGM_Decode_CBP: convexity must be strictly positive.')
+
+nodeBel = UGM_Infer_CBP(convexity,nodePot,edgePot,edgeStruct,inferFunc,varargin{:});
+[~,nodeLabels] = max(nodeBel,[],2);
---
+++ UGM/infer/UGM_Infer_CBP.m
@@ -0,0 +1,22 @@
+function [nodeBel,edgeBel,logZ] = UGM_Infer_CBP(convexity,nodePot,edgePot,edgeStruct,inferFunc,varargin)
+%
+% Convexified BP inference.
+%
+% convexity : scales modulus of convexity of entropy (approximation) by
+%				inverse scaling of log-potentials
+% nodePot : node potentials
+% edgePot : edge potentials
+% edgeStruct : edge struct
+% inferFunc : inference function
+
+assert(nargin >= 5, 'USAGE: UGM_Infer_CBP(convexity,nodePot,edgePot,edgeStruct,inferFunc)')
+
+assert(convexity > 0, 'In UGM_Infer_CBP: convexity must be strictly positive.')
+
+if nargout == 1
+	[nodeBel] = inferFunc(nodePot.^(1/convexity),edgePot.^(1/convexity),edgeStruct,varargin{:});
+elseif nargout == 2
+	[nodeBel,edgeBel] = inferFunc(nodePot.^(1/convexity),edgePot.^(1/convexity),edgeStruct,varargin{:});
+else
+	[nodeBel,edgeBel,logZ] = inferFunc(nodePot.^(1/convexity),edgePot.^(1/convexity),edgeStruct,varargin{:});
+end
