385f1ac7f879e0c4de11929280e278db2905b6e0
Bert Huang <bert@cs.umd.edu>
2014-05-16 18:50:38 -0400

    fixed edge belief computation in schwing's algorithm

--- UGM/infer/UGM_Infer_CountBP.m
+++ UGM/infer/UGM_Infer_CountBP.m
@@ -43,8 +43,8 @@ else
     
 	% Compute messages
 	[imsg,omsg] = UGM_CountBP_schwing(nodePot,edgePot,nodeCount,edgeCount,edgeStruct,momentum,convTol,0);
-	
-	% Compute nodeBel
+
+    % Compute nodeBel
 	nodeBel = zeros(nNodes,maxState);
 	for n = 1:nNodes
         
@@ -79,10 +79,8 @@ else
 		for e = 1:nEdges
 			n1 = edgeEnds(e,1);
 			n2 = edgeEnds(e,2);
-% 			eb = edgePot(1:nStates(n1),1:nStates(n2),e).^(1/edgeCount(e)) .* ...
-% 				 (omsg(1:nStates(n1),e) * omsg(1:nStates(n2),e+nEdges)');
-			eb = edgePot(1:nStates(n1),1:nStates(n2),e) .* ...
-				 (omsg(1:nStates(n1),e) * omsg(1:nStates(n2),e+nEdges)');
+			eb = edgePot(1:nStates(n1),1:nStates(n2),e).^(1/edgeCount(e)) .* ...
+				 (omsg(1:nStates(n1),e) * omsg(1:nStates(n2),e+nEdges)').^(1/edgeCount(e));
 			eb(~isfinite(eb)) = 0;
 			
 			% Safe normalize
