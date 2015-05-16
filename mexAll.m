0a49466543fd92705e11a0fa7cd4e2489048f754
Ben London <blondon@cs.umd.edu>
2015-05-16 15:50:17 -0400

    Pruned out modules not used by london-icml15

--- mexAll.m
+++ mexAll.m
@@ -6,29 +6,6 @@ mex -outdir minFunc/compiled minFunc/lbfgsC.c
 mex -outdir minFunc/compiled minFunc/lbfgsAddC.c
 mex -outdir minFunc/compiled minFunc/lbfgsProdC.c
 
-% KPM
-fprintf('Compiling KPM files...\n');
-mkdir KPM/compiled
-mex -IKPM -outdir KPM/compiled KPM/max_mult.c
-
-% DAGlearn
-fprintf('Compiling DAGlearn files...\n');
-mkdir DAGlearn/ancestorMatrix/compiled
-mex -IDAGlearn/ancestorMatrix -outdir DAGlearn/ancestorMatrix/compiled DAGlearn/ancestorMatrix/ancestorMatrixAddC_InPlace.c
-mex -IDAGlearn/ancestorMatrix -outdir DAGlearn/ancestorMatrix/compiled DAGlearn/ancestorMatrix/ancestorMatrixBuildC.c
-
-% L1GeneralOverlapping Group
-fprintf('Compiling L1GeneralGroup files...\n');
-mkdir L1GeneralGroup/compiled
-mex -outdir L1GeneralGroup/compiled L1GeneralGroup/mex/projectRandom2C.c
-mex -outdir L1GeneralGroup/compiled L1GeneralGroup/mex/auxGroupLinfProjectC.c
-mex -outdir L1GeneralGroup/compiled L1GeneralGroup/mex/auxGroupL2ProjectC.c
-
-% L1GeneralOverlapping Group
-fprintf('Compiling L1GeneralOverlapping Group files...\n');
-mkdir L1GeneralOverlappingGroup/compiled
-mex -outdir L1GeneralOverlappingGroup/compiled L1GeneralOverlappingGroup/projectNDgroup_DykstraFastC.c
-
 % UGM
 fprintf('Compiling UGM files...\n');
 mkdir UGM/compiled
@@ -56,56 +33,7 @@ mex -IUGM/mex -outdir UGM/compiled UGM/mex/UGM_CRF_NLLC.c
 mex -IUGM/mex -outdir UGM/compiled UGM/mex/UGM_Decode_ChainC.c
 mex -IUGM/mex -outdir UGM/compiled UGM/mex/UGM_makeCRFmapsC.c
 
-% LLM2
-fprintf('Compiling LLM2 files...\n');
-mkdir LLM2/compiled
-mex -outdir LLM2/compiled LLM2/mex/LLM2_inferC.c
-mex -outdir LLM2/compiled LLM2/mex/LLM2_suffStatC.c
-mex -outdir LLM2/compiled LLM2/mex/LLM2_pseudoC.c
-
-% LLM
-fprintf('Compiling LLM files...\n');
-mkdir LLM/compiled
-mex -outdir LLM/compiled LLM/mex/LLM_inferC.c
-mex -outdir LLM/compiled LLM/mex/LLM_suffStatC.c
-mex -outdir LLM/compiled LLM/mex/LLM_pseudoC.c
-
-% UGMep
-fprintf('Compiling UGMep files\n');
-mkdir UGMep/compiled
-mex -outdir UGMep/compiled UGMep/mex/UGMep_Decode_ICMC.c
-mex -outdir UGMep/compiled UGMep/mex/UGMep_EnergyC.c
-mex -outdir UGMep/compiled UGMep/mex/UGMep_makeClampedEnergyC.c
-mex -outdir UGMep/compiled UGMep/mex/UGMep_Decode_GraphCutC.c
-mex -outdir UGMep/compiled UGMep/mex/UGMep_Decode_AlphaExpansionC.c
-mex -outdir UGMep/compiled UGMep/mex/UGMep_Decode_ExpandShrinkC.c
-
-% Ewout
-fprintf('Compiling Ewout files...\n');
-mkdir Foreign/Ewout/compiled
-mex -IForeign/Ewout -outdir Foreign/Ewout/compiled Foreign/Ewout/projectBlockL1.c Foreign/Ewout/oneProjectorCore.c Foreign/Ewout/heap.c
-mex -IForeign/Ewout -outdir Foreign/Ewout/compiled Foreign/Ewout/projectBlockL2.c
-
 % Misc
 fprintf('Compiling misc files...\n');
 mkdir misc/compiled
 mex -outdir misc/compiled misc/sampleDiscrete_cumsumC.c
-
-% SAG
-fprintf('Compiling SAG files...\n');
-mkdir SAG/compiled
-mex -outdir SAG/compiled SAG/mex/SGD_logistic.c -largeArrayDims
-mex -outdir SAG/compiled SAG/mex/ASGD_logistic.c -largeArrayDims
-mex -outdir SAG/compiled SAG/mex/PCD_logistic.c -largeArrayDims
-mex -outdir SAG/compiled SAG/mex/DCA_logistic.c -largeArrayDims
-mex -outdir SAG/compiled SAG/mex/SAG_logistic.c -largeArrayDims
-mex -outdir SAG/compiled SAG/mex/SAGlineSearch_logistic.c -largeArrayDims
-mex -outdir SAG/compiled SAG/mex/SAG_LipschitzLS_logistic.c -largeArrayDims
-mex -outdir SAG/compiled SAG/mex/SGD_logistic_BLAS.c -largeArrayDims -lmwblas
-mex -outdir SAG/compiled SAG/mex/ASGD_logistic_BLAS.c -largeArrayDims -lmwblas
-mex -outdir SAG/compiled SAG/mex/PCD_logistic_BLAS.c -largeArrayDims -lmwblas
-mex -outdir SAG/compiled SAG/mex/DCA_logistic_BLAS.c -largeArrayDims -lmwblas
-mex -outdir SAG/compiled SAG/mex/SAG_logistic_BLAS.c -largeArrayDims -lmwblas
-mex -outdir SAG/compiled SAG/mex/SAGlineSearch_logistic_BLAS.c -largeArrayDims -lmwblas
-mex -outdir SAG/compiled SAG/mex/SAG_LipschitzLS_logistic_BLAS.c -largeArrayDims -lmwblas
-
